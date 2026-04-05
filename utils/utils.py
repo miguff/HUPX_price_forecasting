e769f497492e96c71d7ff48ce81c0042da6e03f8
Mihályi Géza Ubuntu <mihgeza2000@gmail.com>
2026-04-05 08:05:44 +0200

    LGBM and xgboost added

--- utils/utils.py
+++ utils/utils.py
@@ -4,13 +4,13 @@ def load_data(synt:str):
 
     match synt:
         case "real":
-            all_data_set = pd.read_csv(r"processed_data\\Processed_data_real.csv", index_col=0)
+            all_data_set = pd.read_csv(r"processed_data//Processed_data_real.csv", index_col=0)
         case "lgbm":
-            all_data_set = pd.read_csv(r"processed_data\\Processed_data_all.csv", index_col=0)
+            all_data_set = pd.read_csv(r"processed_data//Processed_data_all.csv", index_col=0)
         case "intra":
-            all_data_set = pd.read_csv(r"processed_data\\Intra_Pattern_Processed_data_all.csv", index_col=0)
+            all_data_set = pd.read_csv(r"processed_data//Intra_Pattern_Processed_data_all.csv", index_col=0)
         case "spline":
-            all_data_set = pd.read_csv(r"processed_data\\Spline_Processed_data_all.csv", index_col=0)
+            all_data_set = pd.read_csv(r"processed_data//Spline_Processed_data_all.csv", index_col=0)
     
 
     return all_data_set
